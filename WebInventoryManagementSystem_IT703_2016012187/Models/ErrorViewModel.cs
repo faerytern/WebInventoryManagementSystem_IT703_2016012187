c5260546aebea9bf4bc637fa00f2d074b7ab7d84
Faerytern <kara.heffernan.nz@gmail.com>
2022-09-14 23:13:54 +1200

    Add project files.

---
+++ WebInventoryManagementSystem_IT703_2016012187/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebInventoryManagementSystem_IT703_2016012187.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
