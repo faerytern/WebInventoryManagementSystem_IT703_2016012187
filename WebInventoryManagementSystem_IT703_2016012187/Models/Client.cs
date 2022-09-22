734f6740cf8fdee750e67494ec053c228a1b3ee3
faerytern <kara.heffernan.nz@gmail.com>
2022-09-22 15:39:45 +1200

    Tried to do phone number validation stuff. Pain in the ass. Why have the datatype if theres no validation or way to override it!? Oh well.

--- WebInventoryManagementSystem_IT703_2016012187/Models/Client.cs
+++ WebInventoryManagementSystem_IT703_2016012187/Models/Client.cs
@@ -9,7 +9,13 @@ namespace WebInventoryManagementSystem_IT703_2016012187.Models
         public string Address { get; set; }
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+
         [DataType(DataType.PhoneNumber)]
+        [MinLength(8,ErrorMessage ="Phone numbers have to be a minimum of 8 characters.")]
+        [MaxLength(11, ErrorMessage ="Phone numbers cant be more than 11 characters.")]
+        [Phone]
+        [DisplayFormat(DataFormatString = "{0:###-###-####}",ApplyFormatInEditMode = true)]
         public string Phone { get; set; }
         [DataType(DataType.PostalCode)]
         public string PostCode { get; set; }
